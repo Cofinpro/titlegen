536951d0952c4dacdf9903f309171838c3cabba6
Gregor Tudan <gregor.tudan@cofinpro.de>
2017-10-04 07:07:08 +0200

    enable threading

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ from titlegen import app as application
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    application.run(host='0.0.0.0', port=port)
+    application.run(threaded=True, host='0.0.0.0', port=port)
